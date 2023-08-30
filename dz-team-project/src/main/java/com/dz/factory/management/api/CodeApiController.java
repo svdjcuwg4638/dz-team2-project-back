0884cb1c8cf454f7fe08f716010bc340a920df36
svdjcuwg4638 <kdhwon1@gmail.com>
2023-08-30 12:32:13 +0900

    ewfg

--- dz-team-project/src/main/java/com/dz/factory/management/api/CodeApiController.java
+++ dz-team-project/src/main/java/com/dz/factory/management/api/CodeApiController.java
@@ -44,11 +44,6 @@ public class CodeApiController {
 		codeService.addManageCode(data);
 		return new ResponseEntity<>(new CMRespDto<>(1,"성공",data),HttpStatus.OK);
 	}
-	
-	@PostMapping("/managecode/delete")
-	public ResponseEntity<?> manageCodeAdd(@RequestBody ManageCode data){
-		codeService.addManageCode(data);
-		return new ResponseEntity<>(new CMRespDto<>(1,"성공",data),HttpStatus.OK);
-	}
+
 	
 }
