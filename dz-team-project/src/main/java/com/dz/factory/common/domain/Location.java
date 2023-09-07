45ad70363dee3a94721c188785408555092b0650
svdjcuwg4638 <kdhwon1@gmail.com>
2023-09-07 13:35:00 +0900

    db변동사항적용

---
+++ dz-team-project/src/main/java/com/dz/factory/common/domain/Location.java
@@ -0,0 +1,16 @@
+package com.dz.factory.common.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Location {
+	private String location_code;
+	private int company_id;
+	private String storage_code;
+
+	private String location_name;
+}
