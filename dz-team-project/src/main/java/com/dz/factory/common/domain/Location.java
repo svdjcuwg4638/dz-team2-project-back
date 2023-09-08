fdaa74fb21db2db22d492dd9100b4daa7df3875b
ssu <gra.cce.ful@gmail.com>
2023-09-08 16:41:44 +0900

    locationdto >> location  변경 반영

--- dz-team-project/src/main/java/com/dz/factory/common/domain/Location.java
+++ dz-team-project/src/main/java/com/dz/factory/common/domain/Location.java
@@ -7,20 +7,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-<<<<<<< HEAD:dz-team-project/src/main/java/com/dz/factory/management/dto/LocationDto.java
-public class LocationDto {
-//	private int storage_id;
-=======
+
 public class Location {
 	private String location_code;
 	private int company_id;
 	private String storage_code;
->>>>>>> 2eb08d7b591d0dbe0f64dcdd5647558194b5f74d:dz-team-project/src/main/java/com/dz/factory/common/domain/Location.java
-
 	private String location_name;
-<<<<<<< HEAD:dz-team-project/src/main/java/com/dz/factory/management/dto/LocationDto.java
 	private String storage_name;
-	private String storage_code;
-=======
->>>>>>> 2eb08d7b591d0dbe0f64dcdd5647558194b5f74d:dz-team-project/src/main/java/com/dz/factory/common/domain/Location.java
 }
