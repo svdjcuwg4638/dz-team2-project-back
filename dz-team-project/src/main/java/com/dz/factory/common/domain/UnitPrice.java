db33e4d72b24a14c1e8b11c8d2ee8c16b5280bb3
svdjcuwg4638 <kdhwon1@gmail.com>
2023-09-24 00:51:59 +0900

    창고단가 에러처리

--- dz-team-project/src/main/java/com/dz/factory/common/domain/UnitPrice.java
+++ dz-team-project/src/main/java/com/dz/factory/common/domain/UnitPrice.java
@@ -2,24 +2,32 @@ package com.dz.factory.common.domain;
 
 import java.time.LocalDate;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-@Builder
 public class UnitPrice {
 
 	private int unit_price_id;
+	
+	@NotEmpty(message="itme_code")
 	private String item_code;
+	@NotEmpty(message="partner_code")
 	private String partner_code;
+	@NotEmpty(message="unit_price")
 	private String unit_price;
+	@NotNull(message="start_date")
 	private LocalDate start_date;
 	private LocalDate end_date;
+	@NotEmpty(message="type")
 	private String type;
 	private int company_id;
+	private int isDelete;
 	
 }
