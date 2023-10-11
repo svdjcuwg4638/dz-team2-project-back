97303226d27ee83cae1f64788ff19b6a8f45355d
svdjcuwg4638 <kdhwon1@gmail.com>
2023-10-11 20:04:27 +0900

    at

---
+++ dz-team-project/src/main/java/com/dz/factory/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.dz.factory.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+		.allowedOrigins("https://localhost:9091")
+		.allowedMethods("*")
+		.allowedHeaders("*")
+		.allowCredentials(true);
+	}
+}
